82721b7dd16021e1388440053d306fbd82b00796
Khyatee Desai <Khyatee1@0587385053.wireless.umich.net>
2017-04-16 18:19:23 -0400

    everything done except visualization

--- 330final.py
+++ 330final.py
@@ -7,8 +7,8 @@ secret = 'soFn6mtJXNCBFV0cJMrPBOGROwM798TTVGUSjHBLJCwhiUAbcv'
 access_token = '743466833177681922-cRuAJGl4AuO6Wi7PipIiq1ARpBgGeno'
 access_secret = 'kBAlSNgRyaEmHZ0yh6ZfEnG2iNO1NLJqTI6LaenKxCfRQ'
 
-# ------------------------------------ STEP 1 -----------------------------------------------
-# This step scrapes names of current congress members and appends them to the list, "members"
+# ------------------------------------ STEP 1: SCRAPE-----------------------------------------------
+# This step scrapes names of current senators and appends them to the list, "members"
 
 
 # base_url = 'https://ballotpedia.org/List_of_current_members_of_the_U.S._Congress'
@@ -21,8 +21,8 @@ access_secret = 'kBAlSNgRyaEmHZ0yh6ZfEnG2iNO1NLJqTI6LaenKxCfRQ'
 members=[]
 lis=[]
 
-r = open('step1.html','r')
-soup = BeautifulSoup(r, "html.parser")
+t = open('step1.html','r')
+soup = BeautifulSoup(t, "html.parser")
 table = soup.table.find_all('tr')
 for person in table:
     tups=[]
@@ -31,16 +31,24 @@ for person in table:
     	tups.append((lis[1], lis[2]))
     	members.append(tups)
 
-#--------------------------------------- STEP 2 ------------------------------------
+#--------------------------------------- STEP 2: PARTIES------------------------------------
 # this step splits members into two lists: democrats and republicans.
+dems=[]
+repubs = []
+for person in members:
+	name = person[0][0]
+	if "Democratic" in person[-1][-1]:
+		x = re.search('\D+', name)
+		dems.append(x.group(0))
+	else:
+		x = re.search('\D+', name)
+		repubs.append(x.group(0))
 
 
 
 
-
-
-#------------------------------------------ STEP 3 ---------------------------------
-# This step searches twitter api for the names and retrieves a twitter handle, writing it to the file "usernames.txt"
+#------------------------------------------STEP 3: HANDLES---------------------------------
+# This step searches twitter api for the names and retrieves a twitter handle, writing it to either "dem_handles.txt" or "repub_handles.txt"
 
 
 oauth = requests_oauthlib.OAuth1Session(key,
@@ -49,8 +57,19 @@ oauth = requests_oauthlib.OAuth1Session(key,
                         resource_owner_secret=access_secret)
 
 
-# t = open('usernames.txt', 'w')
-# for name in members:
+								#================ Democrat Handles  ======================
+# t = open('dem_handles.txt', 'w')
+# for name in dems:
+# 	r = oauth.get("https://api.twitter.com/1.1/users/search.json?", params = {'q': name, 'count': 1})
+# 	data =r.json()
+# 	for dic in data:
+# 		print(dic['screen_name'])
+# 		t.write(dic['screen_name'])
+# 		t.write(',')
+# t.close()
+								#================ Republican Handles ===================
+# t = open('repub_handles.txt', 'w')
+# for name in repubs:
 # 	r = oauth.get("https://api.twitter.com/1.1/users/search.json?", params = {'q': name, 'count': 1})
 # 	data =r.json()
 # 	for dic in data:
@@ -60,17 +79,77 @@ oauth = requests_oauthlib.OAuth1Session(key,
 # t.close()
 
 
-# ------------------------------------------ STEP 4 ---------------------------------------
+#------------------------------------------ STEP 4: TWEETS--------------------------------------------------------
+# use the democratic and republican twitter handles to make individual search requests to retrieve that person's top 5 tweets, and append to text files "dem_tweets.txt" and "repub_tweets.txt"
+							
+						#========================= Democrat Tweets =====================
+infile = open('dem_handles.txt', 'r')
+outfile = open('dem_tweets.txt', 'w')
+text = infile.read()
+handles = text.split(',')
+for handle in handles:
+	r = oauth.get("https://api.twitter.com/1.1/search/tweets.json?", params = {'q':  handle , 'count':5, 'result_type': 'popular'  })
+	data = r.json()
+	try:
+		for status in data['statuses']:
+			outfile.write(status['text']+'\n')
+	except:
+		print('failed')
+outfile.close()
+infile.close()
 
-# t = open('usernames.txt', 'r')
-# text = t.read()
+
+# infile = open('dem_tweets.txt', 'r')
+# text = infile.read()
+# all_dem_tweets = text.split('\n')
+# infile.close()
+
+
+						#=============== Republican Tweets ===================
+# infile = open('repub_handles.txt', 'r')
+# outfile = open('repub_tweets.txt', 'w')
+# text = infile.read()
 # handles = text.split(',')
-# handle = handles[3]
-# r = oauth.get("https://api.twitter.com/1.1/search/tweets.json?", params = {'q':  handle , 'count':5, 'result_type': 'popular'  })
-# print(r.json())
+# for handle in handles:
+# 	r = oauth.get("https://api.twitter.com/1.1/search/tweets.json?", params = {'q':  handle , 'count':5, 'result_type': 'popular'  })
+# 	data = r.json()
+# 	try:
+# 		for status in data['statuses']:
+# 			outfile.write(status['text']+'\n')
+# 	except:
+# 		print('failed')
+# outfile.close()
+# infile.close()
 
 
-# get top hashtags per person
-# distinguish democrats and republicans
+
+# infile = open('repub_tweets.txt', 'r')
+# text = infile.read()
+# all_repub_tweets = text.split('\n')
+# infile.close()
+
+#-------------------------------------------STEP 5: CODING-----------------------------------------
+
+
+# keyword = input("Enter a word you want to search: ")
+# dem_count=0
+# for tweet in all_dem_tweets:
+# 	if keyword.lower() in tweet.lower():
+# 		dem_count+=1
+# repub_count=0
+# for tweet in all_repub_tweets:
+# 	if keyword.lower() in tweet.lower():
+# 		repub_count+=1
+
+# print("Dem Tweets:",dem_count)
+# print("Repub Tweets:", repub_count)
+
+
+
+
+
+
+
+
 
 
